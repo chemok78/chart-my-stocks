129033c6fa4708e0468ad8b1c4c9a49e9be83db8
Che Mok <chemok78@gmail.com>
2016-10-14 10:09:29 +0000

    add stock function

--- public/js/app.js
+++ public/js/app.js
@@ -19,116 +19,151 @@ angular.module("stocksApp", ['ngRoute', 'highcharts-ng'])
 
     var url = "quotes/";
 
+
     this.retrieveQuotes = function() {
 
       return $http.get(url);
 
     };
-
+    
+    this.addQuote = function(query){
+      
+      var addUrl = "addquotes/" + query;
+      
+      return $http.get(addUrl);
+      
+    };
 
   })
   .controller("mainController", function($scope, Stocks) {
 
-    //function for on load the stocks
-    //looks in database which stocks are searched
-    //calls Yahoo Finance API and sets scope
+    Stocks.retrieveQuotes()
+      .then(function(response) {
+        //when page loads and mainController, automatically call REST API to retrieve an object with stocks data  
 
-    //check in MyVotely how charts are implemented
-
-    //always get the data back in an array of objects
-    
-  Stocks.retrieveQuotes()
-    .then(function(response){
-    //when page loads and mainController, automatically call REST API to retrieve an object with stocks data  
-      
         $scope.stocks = response.data;
         
-        $scope.chartConfig = {
-        options: {
-            chart: {
-                zoomType: 'x'
-            },
-            rangeSelector: {
-                enabled: true,
-                selected: 4
-            },
-            navigator: {
-                enabled: true
-            }
-        },
-        series: $scope.stocks,
-        title: {
-            text: 'Chart My Stocks'
-        },
-        useHighStocks: true
-    };
-      
-    }, function(response){
-      
-        console.log("Could not retrieve stocks data");
-      
-    });
-
-   /*$scope.chartConfig.series.push({
-        name: 'Apple',
-        data: [
-            [1147651200000, 23.15],
-            [1147737600000, 23.01],
-            [1147824000000, 22.73],
-            [1147910400000, 22.83],
-            [1147996800000, 22.56],
-            [1148256000000, 22.88],
-            [1148342400000, 22.79],
-            [1148428800000, 23.50],
-            [1148515200000, 23.74],
-            [1148601600000, 23.72],
-            [1148947200000, 23.15],
-            [1149033600000, 22.65]
-        ]
-    }, {
-        name: 'Google',
-        data: [
-            [1147651200000, 25.15],
-            [1147737600000, 25.01],
-            [1147824000000, 25.73],
-            [1147910400000, 25.83],
-            [1147996800000, 25.56],
-            [1148256000000, 25.88],
-            [1148342400000, 25.79],
-            [1148428800000, 25.50],
-            [1148515200000, 26.74],
-            [1148601600000, 26.72],
-            [1148947200000, 26.15],
-            [1149033600000, 26.65]
-        ]
-
-    }
-
-    );*/
-
-
-   
-    $scope.getQuotes = function() {
-      //function when user clicks search button
-      //gets stock quotes with symbols directly from Yahoo Finance API and updates $scope
-
-      Stocks.retrieveQuotes()
-        .then(function(response) {
+        //create an array of symbols here to display in the front-end
+        
+        $scope.symbols = $scope.stocks.map(function(value, index){
           
-          //edit data object and use it as series property in Highcharts
-
-          $scope.stocks = response.data;
-
-        }, function(response) {
-
-          alert("Error retrieving stock quotes");
-
-          console.log(response);
-
+            return value.name;
+          
         });
 
+        $scope.chartConfig = {
+          options: {
+            chart: {
+              zoomType: 'x'
+            },
+            rangeSelector: {
+              enabled: true,
+              selected: 4
+            },
+            navigator: {
+              enabled: true,
+              series: $scope.stocks
+            }
+          },
+          series: $scope.stocks,
+          title: {
+            text: 'Chart My Stocks'
+          },
+          useHighStocks: true
+        };
 
+      }, function(response) {
+
+        console.log("Could not retrieve stocks data");
+
+      });
+
+    /*$scope.chartConfig.series.push({
+         name: 'Apple',
+         data: [
+             [1147651200000, 23.15],
+             [1147737600000, 23.01],
+             [1147824000000, 22.73],
+             [1147910400000, 22.83],
+             [1147996800000, 22.56],
+             [1148256000000, 22.88],
+             [1148342400000, 22.79],
+             [1148428800000, 23.50],
+             [1148515200000, 23.74],
+             [1148601600000, 23.72],
+             [1148947200000, 23.15],
+             [1149033600000, 22.65]
+         ]
+     }, {
+         name: 'Google',
+         data: [
+             [1147651200000, 25.15],
+             [1147737600000, 25.01],
+             [1147824000000, 25.73],
+             [1147910400000, 25.83],
+             [1147996800000, 25.56],
+             [1148256000000, 25.88],
+             [1148342400000, 25.79],
+             [1148428800000, 25.50],
+             [1148515200000, 26.74],
+             [1148601600000, 26.72],
+             [1148947200000, 26.15],
+             [1149033600000, 26.65]
+         ]
+
+     }
+
+     );*/
+     
+
+    $scope.addSymbol = function(query) {
+      //function when user clicks search button for a stock symbol
+      //gets stock quotes with symbols directly from Yahoo Finance API and updates $scope
+
+      console.log(query);
+      
+      //check with Yahoo API if the symbol exists
+      
+      Stocks.addQuote(query)
+        .then(function(response){
+          
+          if(response.data.length === 0){
+          //if the Web API returns an empty array, the stock symbol does not exist  
+            
+            $scope.notexist = "Stock symbol does not exist, please try again!";
+            //custom error message shown in front-end
+          
+            
+          } else {
+            
+            $scope.notexist = "";
+            //reset custom error message in front-end
+            
+            $scope.chartConfig.series.push(response.data);
+            //add the fetched historical data array for symbol to the chartConfig series array
+            
+            $scope.symbols.push(response.data.name);
+            //add the symbol to be listed in front-end
+            
+          } 
+          
+          
+        }, function(response){
+          
+          console.log("Error finding the stock")
+          
+        });
+      
+    };
+    
+    $scope.removeSymbol = function(symbol){
+    //method to remove stock from database and scope when user clicks "x" 
+      
+      console.log(symbol);
+      
+      
     };
 
 
   });
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 require("dotenv").config({
   silent: true
-})
+});
 
 var path = require("path");
 //use node.js native path module to work with files and paths
@@ -54,6 +54,32 @@ mongodb.MongoClient.connect(process.env.DB_URL, function(err, database) {
     console.log("App is now running on port", port);
 
   });
+  
+   /*Date Parsing to get right format Yahoo API call*/
+
+        var now = new Date();
+        //get the current Date
+
+        var convertDate = function(date) {
+          //function to convert a date to year-month-day format		
+          return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+
+        };
+
+        var nowDate = convertDate(now);
+          //format the date to year-month-day
+
+        var unixDate = now.getTime();
+        //convert the current date to Unix time
+
+        var unixOneYear = unixDate - 157784760000;
+        //Extract the milliseconds from 5 years 
+
+        var dateOneYear = new Date(unixOneYear);
+        //convert Unix time back to date
+
+        var dateOneYearAgo = convertDate(dateOneYear);
+        //format the date to year-month-day
 
   /*RESTFUL API Web services*/
 
@@ -72,30 +98,24 @@ mongodb.MongoClient.connect(process.env.DB_URL, function(err, database) {
 
   /*Routes to get stock data*/
 
-  //stock symbols send from Angular JS front end
-  //retrieve data from Yahoo Finance API
-  //save data in database
-  //send a response: array of data to handle in Angular JS front end
-  //bind data to the $scope, show in Highcharts
-
   app.get("/quotes/", function(req, res) {
-  //API to get stock symbols from database, call Yahoo Finance API and return data as JSON object tp front end
-
+    //API to get stock symbols from database, call Yahoo Finance API and return data as JSON object tp front end
     //look in the database and see which stocks are currently set in the database, using Stock Symbols
 
     var SYMBOLS = [];
     //array of all the stocks to be checked.
     //get the list of stocks from the database
+    
 
     db.collection(STOCKS_COLLECTION).find({}).toArray(function(err, doc) {
-     //look in the database and see which stocks are currently set in the database, using Stock Symbols
+      //look in the database and see which stocks are currently set in the database, using Stock Symbols
 
       if (err) {
 
         handleError(res, err.message, "Failed to find stock symbols in database");
 
       } else {
-      //found the symbols in database  
+        //found the symbols in database  
 
         for (var i = 0; i < doc.length; i++) {
         //create array with only the symbols
@@ -103,41 +123,14 @@ mongodb.MongoClient.connect(process.env.DB_URL, function(err, database) {
           SYMBOLS.push(doc[i].symbol);
 
         }
-        
-        var now = new Date();
-//get the current Date
-
-var convertDate = function(date){
-//function to convert a date to year-month-day format		
-    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
-
-}
-
-var nowDate = convertDate(now)
-//format the date to year-month-day
-
-var unixDate = now.getTime();
-//convert the current date to Unix time
-
-var unixOneYear = unixDate - 157784760000;
-//Extract the milliseconds from 5 years 
-
-var dateOneYear = new Date(unixOneYear);
-//convert Unix time back to date
-
-var dateOneYearAgo = convertDate(dateOneYear);
-//format the date to year-month-day
-
 
         yahooFinance.historical({
-        //call Yahoo API with symbols array
-        //get the data range 1 year ago till today
-        
+            //call Yahoo API with symbols array
+  
             symbols: SYMBOLS,
             from: dateOneYearAgo,
             to: nowDate
-              //get the data from max 1 year back. So from/to must be generated dynamically
-
+              
           }, function(err, quotes) {
 
             if (err) {
@@ -145,66 +138,138 @@ var dateOneYearAgo = convertDate(dateOneYear);
               handleError(res, err.message, "Failed to find stocks from Yahoo Finance API");
 
             } else {
-            //YAHOO API call was successfull
-            
-              var series = [ ];
-              //series = [{ name: 'AAPL', data: [[91732321, 23.15], [15751761, 24.2567 ]]}, {}, {}]
-              //series is an array of objects & data is an array of arrays
-              //quotes = {"AAPL": [{}, {}, {}], "GOOGL": [{}, {}, {}]}
-              //quotes is an object of objects.
-              console.log(quotes['AAPL'][0]);
-            
-              for (var key in quotes){
-              //keys are Apple, Google, Yahoo etc. All of them contain arrays of objects  
-              //quotes[key] is an array of objects
-     
-                  //console.log(quotes[key][0].date);
-                
-                  var dataSeries = [ ];
-                  
-                  for(var i=0; i<quotes[key].length; i++){
-                    
-                    var date = Date.parse(quotes[key][i].date);
-                    
-                      var object = [date,quotes[key][i].close];
-                    
-                      dataSeries.push(object);
-                    
-                  }
-                  
-                  //console.log(dataSeries);
-                
+              //YAHOO API call was successfull
+
+              var series = [];
+              //construct series array (array of objects for each stock) for HighStocks
+       
+              for (var key in quotes) {
+              //quotes is the JSON object returned from the Yahoo API call
+              //With an object for each stock
+
+                var dataSeries = [];
+                //construct dataSeries object with an array of arrays for each day, used in High Stocks
+
+                for (var i = 0; i < quotes[key].length; i++) {
+                //loop through quotes JSON Object for each key/Stock  
+
+                  var date = Date.parse(quotes[key][i].date);
+
+                  var object = [date, quotes[key][i].close];
+
+                  dataSeries.push(object);
+
+                }
+
                 series.push({
-                  
+
                   name: key,
-                  
+
                   data: dataSeries,
-                  
+
                   tooltip: {
-                  
+
                     valueDecimals: 2
-                
+
                   }
-                  
+
                 });
-                
-                
-              }//for loop
-              
-              //console.log(series);
+
+
+              } //for loop
+
 
               res.status(200).json(series);
 
             }
-          }) // yahooFinance.historical
+          }); // yahooFinance.historical
 
-      } //if else
+      } 
 
 
     }); //db.collection(STOCKS_COLLECTION)
 
 
   }); // app.get("/quotes/"
+  
+  app.get("/addquotes/:query", function(req,res){
+  //API called from Angular JS front end with the stock symbol
+  
+    console.log(req.params.query);
+
+    req.params.query = req.params.query.toUpperCase();
+    //convert stock query to Uppercase first for consistency
+    
+    //check if the stock symbol exists in Yahoo Finance API
+    
+    yahooFinance.historical({
+            //call Yahoo API with symbols array
+  
+            symbol: req.params.query,
+            from: dateOneYearAgo,
+            to: nowDate
+              
+          }, function(err, quotes) {
+
+            if (err) {
+              
+              handleError(res, err.message, "Failed to find stocks from Yahoo Finance API");
+
+            } else {
+              //YAHOO API call was successfull
+              
+              if(quotes.length === 0){
+              //if quotes array is empty, the stock does not exist
+              //send empty array to Angular Front end, where it will handle the error
+              
+              res.status(200).send(quotes);
+              
+              } else {
+              
+              //if quotes array is not empty
+            
+              var dataSeries = [];
+                //construct dataSeries object with an array of arrays for each day, used in High Stocks
+
+                 for (var i = 0; i < quotes.length; i++) {
+                  //loop through quotes JSON Object for each Stock  
+
+                    var date = Date.parse(quotes[i].date);
+
+                    var object = [date, quotes[i].close];
+
+                    dataSeries.push(object);
+
+                } //for loop
+
+                var sendData = {
+
+                  name: req.params.query,
+
+                  data: dataSeries,
+
+                  tooltip: {
+
+                    valueDecimals: 2
+
+                  }
+
+                }; 
+                
+                res.status(200).send(sendData);
+                //send the array with the historical stock data back to Angular JS
+                
+                //edit database here
+                
+                
+              
+              } //if else
+
+            }//function(err, quotes) 
+    
+    }); // yahooFinance.historical
+    
+  }); //app.get("/addquotes/:query"
 
 
 }); //mongodb.MongoClient.connect
